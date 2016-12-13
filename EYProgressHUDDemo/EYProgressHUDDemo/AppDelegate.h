ff767ee8ed9f3e4a8da6eab70072b4979d227c29
lieryang <lieryang@gujiabindeMacBook-Pro.local>
2016-12-13 13:34:23 +0800

    增加demo
    增加demo

---
+++ EYProgressHUDDemo/EYProgressHUDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EYProgressHUDDemo
+//
+//  Created by lieryang on 16/12/13.
+//  Copyright © 2016年 lieryang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
